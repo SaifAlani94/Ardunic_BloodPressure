b7e82302ca722fa81595cf75f247d9b06a4a6e69
Saif muqdad <eng.saifalani7@gmail.com>
2022-02-08 12:50:59 +0300

    Update BLoodPreassure_HWSerial.ino

--- examples/BLoodPreassure_HWSerial/BLoodPreassure_HWSerial.ino
+++ examples/BLoodPreassure_HWSerial/BLoodPreassure_HWSerial.ino
@@ -1,18 +1,18 @@
 #include <Ardunic_BloodPressure.h>
 
-
-BloodPressure info(&Serial2);
+// Define the Hardware serial port for your arduino
+BloodPressure info(&Serial2);  // Hardware serial2 for arduino mega was used as an example.
 
 
 void setup(){
-Serial.begin(9600);
-Serial2.begin(9600);
+Serial.begin(9600);   // define the boudrate for your serial monitor.
+Serial2.begin(9600);  // define the boudrate for Serial2 port to communicate with our device.
 }
 
 void loop(){
-info.update();
-Serial.print("SYS = " + String(info.GetSYS()));
-Serial.print(" DIA = " + String(info.GetDIA()));
-Serial.println(" HR = " + String(info.GetHR()));
+info.update(); // Update info from Blood Pressure Device
+Serial.print("SYS = " + String(info.GetSYS()));  // Get systolic blood pressure.
+Serial.print(" DIA = " + String(info.GetDIA())); // Get diastolic blood pressure.
+Serial.println(" HR = " + String(info.GetHR())); // Get Heart rate.
 delay(1000);
 }
