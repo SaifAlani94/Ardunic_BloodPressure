4114723176110e6fb913fb7b9cc20a6f49515175
Saif muqdad <eng.saifalani7@gmail.com>
2022-02-08 12:47:37 +0300

    Update BLoodPreassure_SWSerial.ino

--- examples/BLoodPreassure_SWSerial.ino
+++ examples/BLoodPreassure_SWSerial.ino
@@ -1,16 +1,16 @@
 #include <Ardunic_BloodPressure.h>
 
-
-BloodPressure info(8, 7); // RX,TX 
+// Define the RX pin & TX pin for your Software Serial Port.
+BloodPressure info(8, 7); // RX pin,TX pin
 
 void setup(){
-Serial.begin(9600);
+Serial.begin(9600);  // define the boudrate for your serial monitor
 }
 
 void loop(){
-info.update();
-Serial.print("SYS = " + String(info.GetSYS()));
-Serial.print(" DIA = " + String(info.GetDIA()));
-Serial.println(" HR = " + String(info.GetHR()));
+info.update(); // Update info from Blood Pressure Device
+Serial.print("SYS = " + String(info.GetSYS()));  // Get systolic blood pressure.
+Serial.print(" DIA = " + String(info.GetDIA())); // Get diastolic blood pressure.
+Serial.println(" HR = " + String(info.GetHR())); // Get Heart rate.
 delay(1000);
 }
